d7f542a97f0f9ac568aeefb4eb50c81e772acfd6
wnbell <wnbell@localhost>
2010-08-06 13:32:07 -0700

    changed 'const T init' argument of exclusive_scan* to 'T init' resolves issue #190

---
+++ thrust/detail/host/partition.h
@@ -0,0 +1,144 @@
+/*
+ *  Copyright 2008-2010 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+
+/*! \file partition.h
+ *  \brief Host implementations of partition functions
+ */
+
+#pragma once
+
+namespace thrust
+{
+namespace detail
+{
+namespace host
+{
+
+template <typename ForwardIterator1,
+          typename ForwardIterator2>
+void iter_swap(ForwardIterator1 iter1, ForwardIterator2 iter2)
+{
+    typedef typename thrust::iterator_value<ForwardIterator1>::type T;
+
+    T temp = *iter1;
+    *iter1 = *iter2;
+    *iter2 =  temp;
+}
+
+template<typename ForwardIterator,
+         typename Predicate>
+  ForwardIterator partition(ForwardIterator first,
+                            ForwardIterator last,
+                            Predicate pred)
+{
+    if (first == last)
+        return first;
+
+    while (pred(*first))
+    {
+        if (++first == last)
+            return first;
+    }
+
+    ForwardIterator next = first;
+
+    while (++next != last)
+    {
+        if (pred(*next))
+        {
+            iter_swap(first, next);
+            ++first;
+        }
+    }
+
+    return first;
+}
+
+
+template<typename ForwardIterator,
+         typename Predicate>
+  ForwardIterator stable_partition(ForwardIterator first,
+                                   ForwardIterator last,
+                                   Predicate pred)
+{
+    typedef typename thrust::iterator_value<ForwardIterator>::type T;
+
+    typedef thrust::host_vector<T>        TempRange;
+    typedef typename TempRange::iterator  TempIterator;
+
+    TempRange temp(first, last);
+
+    for(TempIterator iter = temp.begin(); iter != temp.end(); ++iter)
+    {
+        if (pred(*iter))
+        {
+            *first = *iter;
+            ++first;
+        }
+    }
+
+    ForwardIterator middle = first;
+
+    for(TempIterator iter = temp.begin(); iter != temp.end(); ++iter)
+    {
+        if (!bool(pred(*iter)))
+        {
+            *first = *iter;
+            ++first;
+        }
+    }
+
+    return middle;
+}
+
+
+template<typename ForwardIterator1,
+         typename ForwardIterator2,
+         typename Predicate>
+  ForwardIterator2 stable_partition_copy(ForwardIterator1 first,
+                                         ForwardIterator1 last,
+                                         ForwardIterator2 result,
+                                         Predicate pred)
+{
+    for(ForwardIterator1 iter = first; iter != last; iter++)
+        if(pred(*iter))
+            *result++ = *iter;
+
+    ForwardIterator2 middle = result;
+    
+    for(ForwardIterator1 iter = first; iter != last; iter++)
+        if(!pred(*iter))
+            *result++ = *iter;
+
+    return middle;
+}
+
+template<typename ForwardIterator1,
+         typename ForwardIterator2,
+         typename Predicate>
+  ForwardIterator2 partition_copy(ForwardIterator1 first,
+                                  ForwardIterator1 last,
+                                  ForwardIterator2 result,
+                                  Predicate pred)
+{
+    return thrust::experimental::stable_partition_copy(first, last, result, pred);
+}
+
+} // end namespace host
+} // end namespace detail
+} // end namespace thrust
+
