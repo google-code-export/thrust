291a2eaa975b033c2f7fc02c489ec57df45493ab
jaredhoberock <jaredhoberock@localhost>
2011-09-19 13:36:37 -0700

    Add pointer & reference for each backend. Untested.

---
+++ thrust/detail/config/compiler_fence.h
@@ -0,0 +1,50 @@
+/*
+ *  Copyright 2008-2011 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#pragma once
+
+#include <thrust/detail/config.h>
+
+// msvc case
+#if THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_MSVC
+
+#include <intrin.h>
+#define __thrust_compiler_fence() _ReadWriteBarrier()
+
+// gcc case
+#elif THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_GCC
+
+// guard use of __sync_synchronize from older compilers
+#define THRUST_GCC_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
+#if THRUST_GCC_VERSION >= 40200 // atomic built-ins were introduced ~4.2
+#define __thrust_compiler_fence() __sync_synchronize()
+#else
+// allow the code to compile without any guarantees
+#define __thrust_compiler_fence()
+#endif // THRUST_GCC_VERSION
+
+// clean up after ourselves
+#undef THRUST_GCC_VERSION
+
+// unknown case
+#elif THRUST_HOST_COMPILER == THRUST_HOST_COMPILER_UNKNOWN
+
+// allow the code to compile without any guarantees
+#define __thrust_compiler_fence()
+
+#endif
+
---
+++ thrust/system/omp/memory.h
@@ -0,0 +1,160 @@
+/*
+ *  Copyright 2008-2011 NVIDIA Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*! \file omp/memory.h
+ *  \brief Classes for managing OpenMP-typed memory.
+ */
+
+#pragma once
+
+#include <thrust/detail/config.h>
+#include <thrust/detail/pointer_base.h>
+#include <thrust/detail/reference_base.h>
+#include <thrust/detail/type_traits.h>
+#include <ostream>
+
+namespace thrust
+{
+// put the canonical tag in the same ns as the backend's entry points
+// XXX omp's entry points should be under system, not backend
+namespace detail
+{
+namespace backend
+{
+namespace omp
+{
+
+struct tag {};
+
+} // end omp
+} // end backend
+} // end detail
+
+namespace system
+{
+namespace omp
+{
+
+// alias omp's tag here
+using thrust::detail::backend::omp::tag;
+
+// forward declaration of reference for pointer
+template<typename Element> class reference;
+
+template<typename T>
+  class pointer
+    : public thrust::detail::pointer_base<
+               thrust::omp::pointer<T>,
+               T,
+               thrust::omp::reference<T>,
+               thrust::omp::tag
+             >
+{
+  private:
+    typedef thrust::detail::pointer_base<
+      thrust::omp::pointer<T>,
+      T,
+      thrust::omp::reference<T>,
+      thrust::omp::tag
+    > super_t;
+
+  public:
+    // XXX doxygenate these
+
+    // note that omp::pointer's member functions need __host__ __device__
+    // to interoperate with nvcc + thrust::detail::dereference
+
+    __host__ __device__
+    pointer() : super_t() {}
+
+    template<typename OtherT>
+    __host__ __device__
+    explicit pointer(OtherT *ptr) : super_t(ptr) {}
+
+    template<typename OtherT>
+    __host__ __deivce__
+    pointer &operator=(const pointer<OtherT> &other) : super_t(other) {}
+
+    template<typename OtherT>
+    __host__ __device__
+    pointer &operator=(const pointer<OtherT> &other)
+    {
+      return super_t::operator=(other);
+    }
+}; // end pointer
+
+
+template<typename T>
+  class reference
+    : public thrust::detail::reference_base<
+               thrust::omp::reference<T>,
+               T,
+               thrust::omp::pointer<T>
+             >
+{
+  private:
+    typedef thrust::detail::reference_base<
+      thrust::omp::reference<T>,
+      T,
+      thrust::omp::pointer<T>
+    > super_t;
+
+  public:
+    typedef typename super_t::value_type value_type;
+    typedef typename super_t::pointer    pointer;
+
+    template<typename OtherT>
+    __host__ __device__
+    reference(const reference<OtherT> &other,
+              typename thrust::detail::enable_if_convertible<
+                typename reference<OtherT>::pointer,
+                pointer
+              >::type * = 0)
+      : super_t(other)
+    {}
+
+    __host__ __device__
+    explicit reference(const pointer &ptr)
+      : super_t(ptr)
+    {}
+
+    template<typename OtherT>
+    reference &operator=(const reference<OtherT> &other);
+
+    reference &operator=(const value_type &x);
+}; // end reference
+
+template<typename T>
+__host__ __device__
+void swap(reference<T> &x, reference<T> &y);
+
+} // end omp
+} // end system
+
+// alias omp's tag at top-level
+namespace omp
+{
+
+using thrust::system::omp::tag;
+using thrust::system::omp::pointer;
+using thrust::system::omp::reference;
+
+} // end omp
+
+} // end thrust
+
+#include <thrust/system/omp/detail/memory.inl>
+
